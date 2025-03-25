6f6d9ac4a2171b32267b993c4eea8d7c838355da
Zarif Atique <zarifatique02@gmail.com>
2025-03-25 13:42:29 +1100

    feat: read and print input lines in reverse order

--- reverse.c
+++ reverse.c
@@ -1,7 +1,14 @@
 // your code here
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
-	
+	char msg[128];
+	while (fgets(msg, 127, stdin) != NULL) {
+		int index = strlen(msg) - 1;
+		for(int i = index; i >= 0; i--) {
+			printf("%c", msg[i]);
+		}
+	}
 	return 0;
 }
