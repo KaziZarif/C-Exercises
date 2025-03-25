55da3103ca3f140756dc63d172c6f2d7b21ce37f
Zarif Atique <zarifatique02@gmail.com>
2025-03-25 13:15:00 +1100

    Check for valid n

--- caesar.c
+++ caesar.c
@@ -3,7 +3,12 @@
 
 int main(void) {
 	int n;
+	char msg[128];
 	printf("Enter key: ");
 	scanf("%d", &n);
+	printf("\n");
+	if (n < 0 || n > 26) {
+		printf("Invalid key!\n");
+		return 1;
+	}
 	return 0;
-}
