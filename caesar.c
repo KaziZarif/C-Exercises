6858342a49856b75fb77643f65df016df4c2ad12
Zarif Atique <zarifatique02@gmail.com>
2025-03-25 13:19:32 +1100

    feat: add input prompt and read message into msg using scanf

--- caesar.c
+++ caesar.c
@@ -6,9 +6,12 @@ int main(void) {
 	char msg[128];
 	printf("Enter key: ");
 	scanf("%d", &n);
-	printf("\n");
 	if (n < 0 || n > 26) {
+		printf("\n");
 		printf("Invalid key!\n");
 		return 1;
 	}
+	printf("Enter line: ");
+	scanf("%127s", msg);
 	return 0;
+}
