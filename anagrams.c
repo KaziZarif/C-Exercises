649bd971308ac1a4e6b9ad65fea91b80c344b7b4
Zarif Atique <zarifatique02@gmail.com>
2025-04-01 13:04:52 +1100

    clean the input and remove non-alphanumeric characters

--- anagrams.c
+++ anagrams.c
@@ -1,4 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int check_anagram(char* str1, char* str2) {
+	char* clean_str1 = malloc(256);
+	char* clean_str2 = malloc(256);
+	int i = 0;
+	while(*str1) {
+		if(isalnum(*str1)) {
+				clean_str1[i++] = tolower(*str1);
+		}
+		str1++;
+	}
+	clean_str1[i] = '\0'
+	int j = 0;
+	while(*str2) {
+		if(isalnum(*str2)) {
+				clean_str2[j++] = tolower(*str2);
+		}
+		str2++;
+	}
+	clean_str2[j] = '\0'
+}
+
 
 int main(void) {
 	char line[256];
@@ -7,6 +31,7 @@ int main(void) {
 	fgets(line, sizeof(line), stdin);
 	printf("Enter anagram: ");
 	fgets(anagram, sizeof(anagram), stdin);
+	
 	
 	return 0;
 }
