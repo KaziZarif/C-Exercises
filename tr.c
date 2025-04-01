071c706c2cbfe640820d5936672172ea89bfd05f
Zarif Atique <zarifatique02@gmail.com>
2025-04-01 12:45:21 +1100

    Implementation of a basic tr command in C

--- tr.c
+++ tr.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+void replace_chars(char* str, char* replace, char* replacement, int length) {
+	while(*str) {
+		for(int i = 0; i < length; i++) {
+			if(*str == replace[i]) {
+				*str = replacement[i];
+				break;
+			}
+		}
+		str++;
+	}
+}
 
 
 int main(int argc, char** argv) {
@@ -19,6 +32,14 @@ int main(int argc, char** argv) {
 	if (strlen(argv[1]) != strlen(argv[2])) {
 		printf("Invalid arguments\n");
 		return 1;
+	}
+	char* replace = argv[1];
+	char* replacement = argv[2];
+	int length = strlen(replace);
+	char buf[256];
+	while(fgets(buf, sizeof(buf), stdin) != NULL) {
+		replace_chars(buf, replace, replacement, length);
+		printf("%s", buf);
 	}
 	return 0;
 }
