565b48f7a2f12fea7438e6504e3a74c29a3b1f7e
Zarif Atique <zarifatique02@gmail.com>
2025-04-03 17:51:05 +1100

    make variable names more distinct and mark symbol in struct as const

--- roman.c
+++ roman.c
@@ -2,6 +2,32 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define ROMAN_NUMBERS 13
+
+typdef struct {
+	const char* symbol;
+	int value;
+} Roman;
+
+const romanSymbols[] = {
+	{"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400},
+    {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40},
+	{"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}};
+					
+void numToRoman(int num) {
+	char* romanNumeral = malloc(64);
+	int pos = 0;
+	while (num > 0) {
+		int i = 0;
+		while(i < ROMAN_NUMBERS && num > 0) {
+			if (num >= romanSymbols[i].value) {
+				strcpy(&romanNumeral[pos], 
+			}
+		}
+	}
+	
+}
+
 int main(void) {
 	char input[100];
     char *endptr;
@@ -18,5 +44,6 @@ int main(void) {
 			printf("\nValid number: %ld\n", num);
 		}
 	}
+	
 	return 0;
 }
