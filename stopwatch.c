4f122029729bf502835217502e7c6ef7b48f18a1
Zarif Atique <zarifatique02@gmail.com>
2025-07-24 12:09:19 +1000

    setup signal handlers

--- stopwatch.c
+++ stopwatch.c
@@ -3,8 +3,23 @@
 #include <signal.h>
 #include <sys/types.h>=
 
+volatile sig_atomic_t start = 0;
 
-struct sigaction sa;
+struct sigaction sa_1;
+struct sigaction sa_2;
+struct sigaction sa_3;
+
+void handle_sigusr1(int sig) {
+
+}
+
+void handle_sigusr2(int sig) {
+
+}
+
+void handle_sigint(int sig) {
+
+}
 
 int main(int argc, char** argv) {
 
@@ -16,7 +31,7 @@ int main(int argc, char** argv) {
     if (id == 0) {
 
     } else {
-        
+
     }
 
     return 0;
