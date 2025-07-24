2eaf09ecf2d9ca3b5443cbcd425a043ba7182e38
Zarif Atique <zarifatique02@gmail.com>
2025-07-24 12:13:35 +1000

    Parent process complete

--- stopwatch.c
+++ stopwatch.c
@@ -10,15 +10,15 @@ struct sigaction sa_2;
 struct sigaction sa_3;
 
 void handle_sigusr1(int sig) {
-
+    start = 1;
 }
 
 void handle_sigusr2(int sig) {
-
+    start = 0;
 }
 
 void handle_sigint(int sig) {
-
+    start = 2;
 }
 
 int main(int argc, char** argv) {
@@ -29,9 +29,26 @@ int main(int argc, char** argv) {
         return 1;
     } 
     if (id == 0) {
+        sa_1.sa_handler = &handle_sigusr1;
+        sigaction
 
     } else {
-
+        while(1) {
+            char c;
+            printf("Press s to start stopwatch, p to pause and q to quit: \n");
+            scanf("%c", &c);
+            if (c == 's') {
+                kill(id, SIGUSR1);
+            } 
+            else if (c == 'p') {
+                kill(id, SIGUSR2);
+            } 
+            else if (c == 'q') {
+                kill(id, SIGINT);
+                break;
+            }
+        }
+        wait(NULL);
     }
 
     return 0;
