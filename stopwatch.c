d96e69ac9dae3d1f3808615f3ac7d17f0196f85c
Zarif Atique <zarifatique02@gmail.com>
2025-07-24 12:06:57 +1000

    setup parent and child process

--- stopwatch.c
+++ stopwatch.c
@@ -9,7 +9,15 @@ struct sigaction sa;
 int main(int argc, char** argv) {
 
     pid_t id = fork();
+    if (id == -1) {
+        printf("Fork failed.\n");
+        return 1;
+    } 
+    if (id == 0) {
 
+    } else {
+        
+    }
 
     return 0;
 }
