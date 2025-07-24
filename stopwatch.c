9c2365afa4d60b8e48b7ad9aa9b40192eabd5511
Zarif Atique <zarifatique02@gmail.com>
2025-07-24 12:06:02 +1000

    Initialize stopwatch program in c

---
+++ stopwatch.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>=
+
+
+struct sigaction sa;
+
+int main(int argc, char** argv) {
+
+    pid_t id = fork();
+
+
+    return 0;
+}
